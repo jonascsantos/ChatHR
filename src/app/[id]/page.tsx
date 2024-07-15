91654dbfb1719c97e10c7080a10c840911bcb665
Jonas dos Santos <jonas.cassiano@hotmail.com>
2024-07-15 16:01:05 +0200

    deployment: generateStaticParams func

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -4,6 +4,10 @@ import { Chat } from "@/components/Chat";
 
 export const dynamicParams = true 
 
+export async function generateStaticParams() {
+  return [];
+}
+
 export default function ChatPage({ params }: { params: { id: string } }) {
   const { id } = params;
 
