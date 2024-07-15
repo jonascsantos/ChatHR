61ce17f51b4a1d900f9dbc7f9e7f67d0cdfda562
Jonas dos Santos <jonas.cassiano@hotmail.com>
2024-07-15 15:46:32 +0200

    deployment: Output mode to Standalone

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: process.env.GITHUB_ACTION ? 'export' : undefined,
+    output: 'standalone'
 };
 
 export default nextConfig;
